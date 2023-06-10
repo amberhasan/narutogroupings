de18b86f563dddd32b73e6031133cfdd92a92b7a
amberhasan <amber.hasan@gmail.com>
2023-06-10 13:34:51 -0500

    Fetching data and badly displaying it

--- screens/Characters.js
+++ screens/Characters.js
@@ -1,12 +1,90 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from 'react-native';
 
 const Characters = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  async function getCharacters() {
+    try {
+      const response = await fetch('https://api.narutodb.xyz/character?page=1');
+      const jsonData = await response.json();
+      setCharacters(jsonData.characters);
+    } catch (error) {
+      console.log('Error fetching characters:', error);
+    }
+  }
+
+  const renderCharacterItem = ({item}) => {
+    const {name, image, gender, rank, ninjaRegistration} = item;
+
+    return (
+      <TouchableOpacity style={styles.characterItem}>
+        <Image source={{uri: image}} style={styles.characterImage} />
+        <View style={styles.characterInfo}>
+          <Text style={styles.characterName}>{name}</Text>
+          <Text style={styles.characterDetails}>Gender: {gender}</Text>
+
+          {/* Add more properties here */}
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View>
-      <Text>Characters Screen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={characters}
+        keyExtractor={item => item.id.toString()}
+        renderItem={renderCharacterItem}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  characterItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 10,
+    padding: 10,
+  },
+  characterImage: {
+    width: 80,
+    height: 80,
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  characterInfo: {
+    flex: 1,
+  },
+  characterName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  characterDetails: {
+    fontSize: 14,
+    marginBottom: 3,
+  },
+});
 
 export default Characters;
--- screens/Clans.js
+++ screens/Clans.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, FlatList, TouchableOpacity} from 'react-native';
 
 const Clans = () => {
+  const [clans, setClans] = useState([]);
+
+  useEffect(() => {
+    getClans();
+  }, []);
+
+  async function getClans() {
+    try {
+      const response = await fetch('https://api.narutodb.xyz/clan');
+      const jsonData = await response.json();
+
+      setClans(jsonData.clans);
+    } catch (error) {
+      console.log('Error fetching characters:', error);
+    }
+  }
+
   return (
-    <View>
-      <Text>Clans Screen</Text>
+    <View style={{flex: 1}}>
+      <FlatList
+        data={clans}
+        keyExtractor={item => item.id.toString()} // Provide a unique identifier for each item
+        renderItem={({item}) => {
+          return (
+            <TouchableOpacity>
+              <Text>{item.name}</Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
     </View>
   );
 };
